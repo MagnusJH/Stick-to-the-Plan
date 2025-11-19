8affec058d6f9e750b69002e0d25a0e290a7078d
J3ss62205 <147669349+J3ss62205@users.noreply.github.com>
2025-11-19 13:36:18 -0800

    Abstract class for food, weight, and cardio

---
+++ FWC.java
@@ -0,0 +1,15 @@
+public abstract class FWC {
+    int row;
+    int table;
+
+    public int getRow() {
+        return row;
+    }
+
+    public int createTable() {
+        return table;
+    }
+
+    public abstract void addRow();
+
+}
