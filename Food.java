ee39d17ecbabc7277817c25f41e1bdd682212b90
J3ss62205 <147669349+J3ss62205@users.noreply.github.com>
2025-11-21 10:02:15 -0800

    Update Food.java
    :)

--- Food.java
+++ Food.java
@@ -1,1 +1,32 @@
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
 
+public class Food extends FWC <FoodEntry>{
+    Connector c = new Connector();
+    Connection con = c.getConnection();
+    private int foodID;
+
+    public Food(Connection con, int foodID) {
+        super(con, foodID);
+    }
+    // INSERT INTO MYSQL TABLE
+    protected void addEntry(FoodEntry entry) {
+        String sql = "INSERT INTO food (foodID, rowID, foodName, cals, nutrNum, nutrName) VALUES (?, ?, ?, ?)";
+
+        try (PreparedStatement stmt = con.prepareStatement(sql)) {
+            stmt.setInt(1,parentID);
+            stmt.setInt(2,entry.getRowID());
+            stmt.setString(3,entry.getFoodName());
+            stmt.setInt(4,entry.getCals());
+            stmt.setInt(4,entry.getNutrNum());
+            stmt.setString(5,entry.getNutrName());
+
+            stmt.executeUpdate();
+            System.out.println("Food row inserted!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
