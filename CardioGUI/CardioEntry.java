9ec24fd17bdb54d319616959fcc6fd24fd11cae0
Caden-web <65564258+Caden-web@users.noreply.github.com>
2025-12-03 10:35:03 -0800

    Updated CardioEntry
    added setters to make table GUI functional

--- CardioGUI/CardioEntry.java
+++ CardioGUI/CardioEntry.java
@@ -1,54 +1,57 @@
 
-package CardioGUI;
+import javafx.beans.property.*;
 
 public class CardioEntry {
-    private int rowID;
-    private int cardioID;
-    private String cName;
-    private double duration;
-    private String durType;
-    private double distance;
-    private String distType;
-    private int caloriesBurned;
 
-    public CardioEntry(int cardioID, int rowID, String cName, double duration, String durType, Double distance, String distType, int caloriesBurned) {
-        this.rowID = rowID;
-        this.cardioID = cardioID;
-        this.cName = cName;
-        this.duration = duration;
-        this.durType = durType;
-        this.distance = distance;
-        this.distType = distType;
-        this.caloriesBurned = caloriesBurned;
+    private final IntegerProperty rowID = new SimpleIntegerProperty();
+    private final IntegerProperty cardioID = new SimpleIntegerProperty();
+    private final StringProperty cName = new SimpleStringProperty();
+    private final DoubleProperty duration = new SimpleDoubleProperty();
+    private final StringProperty durType = new SimpleStringProperty();
+    private final DoubleProperty distance = new SimpleDoubleProperty();
+    private final StringProperty distType = new SimpleStringProperty();
+    private final IntegerProperty caloriesBurned = new SimpleIntegerProperty();
+
+    public CardioEntry(int cardioID, int rowID, String cName,
+                       double duration, String durType,
+                       double distance, String distType, int caloriesBurned) {
+
+        this.cardioID.set(cardioID);
+        this.rowID.set(rowID);
+        this.cName.set(cName);
+        this.duration.set(duration);
+        this.durType.set(durType);
+        this.distance.set(distance);
+        this.distType.set(distType);
+        this.caloriesBurned.set(caloriesBurned);
     }
 
-    public int getRowID() {
-        return rowID;
-    }
+    // Getters for TableView
+    public StringProperty cNameProperty() { return cName; }
+    public DoubleProperty durationProperty() { return duration; }
+    public StringProperty durTypeProperty() { return durType; }
+    public DoubleProperty distanceProperty() { return distance; }
+    public StringProperty distTypeProperty() { return distType; }
+    public IntegerProperty caloriesBurnedProperty() { return caloriesBurned; }
 
-    public String getCName() {
-        return cName;
-    }
+    public int getRowID() { return rowID.get(); }
+    public int getCardioID() { return cardioID.get(); }
+    public String getCName() { return cName.get(); }
+    public double getDuration() { return duration.get(); }
+    public String getDurType() { return durType.get(); }
+    public double getDistance() { return distance.get(); }
+    public String getDistType() { return distType.get(); }
+    public int getCalBurned() { return caloriesBurned.get(); }
 
-    public double getDuration() {
-        return duration;
-    }
 
-    public String getDurType() {
-        return durType;
-    }
-
-    public double getDistance() {
-        return distance;
-    }
-
-    public String getDistType() {
-        return distType;
-    }
-
-    public int getCalBurned() {
-        return  caloriesBurned;
-    }
+    // Setters
+    public void setCName(String v) { cName.set(v); }
+    public void setDuration(double v) { duration.set(v); }
+    public void setDurType(String v) { durType.set(v); }
+    public void setDistance(double v) { distance.set(v); }
+    public void setDistType(String v) { distType.set(v); }
+    public void setCaloriesBurned(int v) { caloriesBurned.set(v); }
+}
 
 
 
