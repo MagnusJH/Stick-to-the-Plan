20e7544cbedd370637c7cc1760cd903dd1b058cf
Cruz Gonzalez <cgonza509@gmail.com>
2025-12-03 22:11:09 -0800

    Update LoginManager.java

--- Login/LoginManager.java
+++ Login/LoginManager.java
@@ -1,12 +1,18 @@
+package Login;
+
+/**
+ * Handles authentication and registration requests.
+ * Acts as a bridge between the GUI controller and the UserDAO.
+ */
 public class LoginManager {
 
-    private UserDAO userDAO = new UserDAO();
+    private UserDAO userDAO = new UserDAO();  // Handles all DB operations
 
     /**
      * Attempts to log in a user.
      *
-     * @param username the entered username
-     * @param password the entered password
+     * @param username entered username
+     * @param password entered password
      * @return true if login is successful
      */
     public boolean login(String username, String password) {
@@ -14,13 +20,14 @@ public class LoginManager {
     }
 
     /**
-     * Attempts to register a user.
+     * Attempts to register a user using a User object.
+     * User.isValid() should be checked before this method is called.
      *
-     * @param username the desired username
-     * @param password the desired password
+     * @param user validated User object
      * @return true if registration succeeds
      */
-    public boolean register(String username, String password) {
-        return userDAO.registerUser(username, password);
+    public boolean register(User user) {
+        return userDAO.registerUser(user.getUsername(), user.getPassword());
     }
 }
+
