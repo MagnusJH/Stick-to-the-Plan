0629af69b966fd61465e9ec9f51a8971fda8d340
J3ss62205 <147669349+J3ss62205@users.noreply.github.com>
2025-12-05 13:13:12 -0800

    Updated FoodEntry.java

--- FoodGUI/FoodEntry.java
+++ FoodGUI/FoodEntry.java
@@ -1,16 +1,18 @@
+package FoodGUI;
+
+import javafx.beans.property.*;
+
 public class FoodEntry {
     private int rowID;
-    private String foodName;
-    private int cals;
-    private int nutrNum;
-    private String nutrName;
+    private final StringProperty foodName;
+    private final IntegerProperty cals;
+    private final IntegerProperty nutrNum;
 
-    public FoodEntry(int rowID, String foodName, int cals, int nutrNum, String nutrName) {
+    public FoodEntry(int rowID, String foodName, int cals, int nutrNum) {
         this.rowID = rowID;
-        this.foodName = foodName;
-        this.cals = cals;
-        this.nutrNum = nutrNum;
-        this.nutrName = nutrName;
+        this.foodName = new SimpleStringProperty(foodName);
+        this.cals = new SimpleIntegerProperty(cals);
+        this.nutrNum = new SimpleIntegerProperty(nutrNum);
     }
 
     public int getRowID() {
@@ -18,34 +20,39 @@ public class FoodEntry {
     }
 
     public String getFoodName() {
-        return foodName;
+        return foodName.get();
     }
 
     public void setFoodName(String f) {
-        foodName = f;
+        foodName.set(f);
+    }
+
+    public StringProperty fNameProperty() {
+        return foodName;
     }
 
     public int getCals() {
-        return cals;
+        return cals.get();
     }
 
     public void setCals(int c) {
-        cals = c;
+        cals.set(c);
     }
 
-    public int getNutrNum() {
-        return nutrNum;
+    public IntegerProperty calsProperty() {
+        return cals;
+    }
+
+    public int getNutrNum() { return nutrNum.get();
     }
 
     public void setNutrNum(int n) {
-        nutrNum = n;
+        nutrNum.set(n);
     }
 
-    public String getNutrName() {
-        return nutrName;
+    public IntegerProperty  nutrNumProperty() {
+        return nutrNum;
     }
 
-    public void setNutrName(String n) {
-        nutrName = n;
-    }
 }
+
