8d4df5741cce1c312e731b6ee19237b22396d7fc
J3ss62205 <147669349+J3ss62205@users.noreply.github.com>
2025-12-05 13:19:13 -0800

    Updated Food.java

--- FoodGUI/Food.java
+++ FoodGUI/Food.java
@@ -1,9 +1,13 @@
+package FoodGUI;
+
+
+import Connector.Connector;
+import Connector.FWC;
 import java.sql.SQLException;
 import java.sql.PreparedStatement;
 import java.sql.Connection;
-import java.sql.ResultSet; 
 
-public class Food extends FWC <FoodEntry>{
+public class Food extends FWC<FoodEntry> {
     Connector c = new Connector();
     Connection con = c.getConnection();
     private int foodID;
@@ -13,20 +17,20 @@ public class Food extends FWC <FoodEntry>{
     }
     // INSERT INTO MYSQL TABLE
     protected void addEntry(FoodEntry entry) {
-        String sql = "INSERT INTO food (foodID, rowID, foodName, cals, nutrNum, nutrName) VALUES (?, ?, ?, ?)";
+        String sql = "INSERT INTO food (foodID, rowID, foodName, cals, nutrNum) VALUES (?, ?, ?, ?, ?)";
 
         try (PreparedStatement stmt = con.prepareStatement(sql)) {
             stmt.setInt(1,parentID);
             stmt.setInt(2,entry.getRowID());
             stmt.setString(3,entry.getFoodName());
             stmt.setInt(4,entry.getCals());
-            stmt.setInt(4,entry.getNutrNum());
-            stmt.setString(5,entry.getNutrName());
+            stmt.setInt(5,entry.getNutrNum());
 
             stmt.executeUpdate();
-            System.out.println("Food row inserted!");
+            System.out.println("FoodGUI.Food row inserted!");
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 }
+
