dc1a39750ce9baf23f0e500b5784ef4a1e9b99d9
Caden-web <65564258+Caden-web@users.noreply.github.com>
2025-12-05 09:28:20 -0800

    Refactor WeightEntry to use JavaFX properties

--- WeightGUI/WeightEntry.java
+++ WeightGUI/WeightEntry.java
@@ -1,37 +1,64 @@
 package WeightGUI;
 
+import javafx.beans.property.*;
+
 public class WeightEntry {
     private int rowID;
-    private int sets;
-    private int reps;
-    private int weight;
-    private String wName;
+    private final StringProperty wName;
+    private final IntegerProperty sets;
+    private final IntegerProperty reps;
+    private final IntegerProperty weight;
 
-    public WeightEntry(int rowID, int sets, int reps, int weight, String wName) {
+    public WeightEntry(int rowID, String wName, int sets, int reps, int weight) {
         this.rowID = rowID;
-        this.sets = sets;
-        this.reps = reps;
-        this.weight = weight;
-        this.wName = wName;
+        this.wName = new SimpleStringProperty(wName);
+        this.sets = new SimpleIntegerProperty(sets);
+        this.reps = new SimpleIntegerProperty(reps);
+        this.weight = new SimpleIntegerProperty(weight);
     }
 
     public int getRowID() {
         return rowID;
     }
 
+    public String getWName() {
+        return wName.get();
+    }
+    public void setWName(String value) {
+        wName.set(value);
+    }
+    public StringProperty wNameProperty() {
+        return wName;
+    }
+
     public int getSets() {
+        return sets.get();
+    }
+    public void setSets(int value) {
+        sets.set(value);
+    }
+    public IntegerProperty setsProperty() {
         return sets;
     }
 
     public int getReps() {
+        return reps.get();
+    }
+    public void setReps(int value) {
+        reps.set(value);
+    }
+    public IntegerProperty repsProperty() {
         return reps;
     }
 
     public int getWeight() {
+        return weight.get();
+    }
+    public void setWeight(int value) {
+        weight.set(value);
+    }
+    public IntegerProperty weightProperty() {
         return weight;
     }
-
-    public String getWName() {
-        return wName;
-    }
 }
+
