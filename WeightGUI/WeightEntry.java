0a55df775f2c932a7c100cac2ecc514095a380d5
J3ss62205 <147669349+J3ss62205@users.noreply.github.com>
2025-12-08 20:11:13 -0800

    Updated WeightEntry.java

--- WeightGUI/WeightEntry.java
+++ WeightGUI/WeightEntry.java
@@ -3,7 +3,7 @@ package WeightGUI;
 import javafx.beans.property.*;
 
 public class WeightEntry {
-    private int rowID;
+    private final int rowID;
     private final StringProperty wName;
     private final IntegerProperty sets;
     private final IntegerProperty reps;
@@ -17,52 +17,22 @@ public class WeightEntry {
         this.weight = new SimpleIntegerProperty(weight);
     }
 
-    public int getRowID() {
-        return rowID;
-    }
+    public int getRowID() { return rowID; }
 
-    public String getWName() {
-        return wName.get();
-    }
-    public void setWName(String value) {
-        wName.set(value);
-    }
+    public String getWName() { return wName.get(); }
+    public void setWName(String value) { wName.set(value); }
+    public StringProperty wNameProperty() { return wName; }
 
-    public StringProperty wNameProperty() {
-        return wName;
-    }
+    public int getSets() { return sets.get(); }
+    public void setSets(int value) { sets.set(value); }
+    public IntegerProperty setsProperty() { return sets; }
 
-    public int getSets() {
-        return sets.get();
-    }
-    public void setSets(int value) {
-        sets.set(value);
-    }
+    public int getReps() { return reps.get(); }
+    public void setReps(int value) { reps.set(value); }
+    public IntegerProperty repsProperty() { return reps; }
 
-    public IntegerProperty setsProperty() {
-        return sets;
-    }
-
-    public int getReps() {
-        return reps.get();
-    }
-    public void setReps(int value) {
-        reps.set(value);
-    }
-
-    public IntegerProperty repsProperty() {
-        return reps;
-    }
-
-    public int getWeight() {
-        return weight.get();
-    }
-    public void setWeight(int value) {
-        weight.set(value);
-    }
-
-    public IntegerProperty weightProperty() {
-        return weight;
-    }
+    public int getWeight() { return weight.get(); }
+    public void setWeight(int value) { weight.set(value); }
+    public IntegerProperty weightProperty() { return weight; }
 }
 
